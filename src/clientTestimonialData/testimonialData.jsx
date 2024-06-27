4de90165a067367b1c094db9b142793c1619653f
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-06-27 13:59:46 +0100

    updated text

--- src/clientTestimonialData/testimonialData.jsx
+++ src/clientTestimonialData/testimonialData.jsx
@@ -2,7 +2,7 @@ const testimoniala_data = [
   {
     avatar: "/Rectangle 10.png",
     company: "Jeep",
-    data: `"The team at Komosu Sales is incredibly knowledgeable and dedicated. Their real-time analytics and reporting tools have given us a deeper understanding of our sales performance and customer behavior. This has enabled us to make smarter, data-driven decisions, resulting in a noticeable boost in sales."`,
+    data: "The team at Komosu Sales is incredibly knowledgeable and dedicated. Their real-time analytics and reporting tools have given us a deeper understanding of our sales performance and customer behavior. This has enabled us to make smarter, data-driven decisions, resulting in a noticeable boost in sales.",
     username: "David Allen",
     role: "MD. Jeep",
   },
