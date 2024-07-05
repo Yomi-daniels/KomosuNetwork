aec25db3698ba57cfbd758dc55dee93645ee7de5
YODA <121935558+Yomi-daniels@users.noreply.github.com>
2024-07-05 09:26:08 +0100

    updating changes

--- src/sections/landingPage.jsx
+++ src/sections/landingPage.jsx
@@ -27,7 +27,7 @@ const LandingPage = () => {
     gsap.fromTo(
       heroTextRef.current,
       { x: -100, scale: 0.5 },
-      { x: -0, scale: 1, opacity: 1, duration: 1, ease: "back.in" }
+      { x: -0, scale: 1, opacity: 1, duration: 1, ease: "power1.inOut" }
     );
     gsap.fromTo(
       heroSubRef.current,
@@ -48,25 +48,20 @@ const LandingPage = () => {
   }, []);
 
   return (
-    <div className={`${styles.heroSection}${montserratFont.className}`}>
-      <video
-        ref={videoRef}
-        className={styles.VideoBg}
-        autoPlay
-        muted
-        playsInline
-      >
-        <source src="/Hero-background.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+    <section className={styles.heroSection}>
+      <div className={styles.heroContainer}>
+        <div className={styles.heroImg}>
+          <Image src="/landing image.png" layout="fill" objectFit="cover"/>
+        </div>
+      </div>
 
       <div className={styles.HeroText}>
-        <p className={styles.heroSub} ref={heroSubRef}>
-          Grow and expand your business with
-        </p>
-        <h1
+      <p className={styles.heroSub}>
+        Grow and expand your business with
+      </p>
+      <h1
           className={`${styles.heroHeader} ${montserratFont.className}`}
-          ref={heroTextRef}
+         
         >
           <span className={styles.heroSpan}>KOMOSU</span> NETWORK
         </h1>
@@ -79,14 +74,54 @@ const LandingPage = () => {
           </div>
         </button>
       </div>
-      <div className={styles.HeaderContent} ref={headerContentRef}>
+      <div className={styles.HeaderContent}>
         <p className={styles.heroSubText}>
           Our team of experts will help provide long lasting solutions for your
           business through web design, AI Solutions, and other high quality
           impressions through content marketing and lead generations.
         </p>
       </div>
-    </div>
+
+    </section>
+    // <div className={`${styles.heroSection}${montserratFont.className}`}>
+    //   <video
+    //     ref={videoRef}
+    //     className={styles.VideoBg}
+    //     autoPlay
+    //     muted
+    //     playsInline
+    //   >
+    //     <source src="/Hero-background.mp4" type="video/mp4" />
+    //     Your browser does not support the video tag.
+    //   </video>
+   
+    //   <div className={styles.HeroText}>
+    //     <p className={styles.heroSub} ref={heroSubRef}>
+    //       Grow and expand your business with
+    //     </p>
+    //     <h1
+    //       className={`${styles.heroHeader} ${montserratFont.className}`}
+    //       ref={heroTextRef}
+    //     >
+    //       <span className={styles.heroSpan}>KOMOSU</span> NETWORK
+    //     </h1>
+    //     <button className={styles.headerbtn} ref={buttonRef}>
+    //       Learn more
+    //       <div className={styles.buttonImageBg}>
+    //         <div className={styles.buttonImage}>
+    //           <Image src="/Vector.png" alt="vector" fill />
+    //         </div>
+    //       </div>
+    //     </button>
+    //   </div>
+    //   <div className={styles.HeaderContent} ref={headerContentRef}>
+    //     <p className={styles.heroSubText}>
+    //       Our team of experts will help provide long lasting solutions for your
+    //       business through web design, AI Solutions, and other high quality
+    //       impressions through content marketing and lead generations.
+    //     </p>
+    //   </div>
+    // </div>
   );
 };
 
